34953bc44936705f09aa26cc4c94f41398eecae1
Matthew Berryman <mattjb@me.com>
2017-04-10 14:46:38 +1000

    schema for pg database

---
+++ schema.sql
@@ -0,0 +1,14 @@
+-- table for holding processed outputs
+
+CREATE EXTENSION postgis;
+
+CREATE TABLE processed_data (
+pkey serial,
+measureDateTime timestamp with time zone NOT NULL,
+location geometry(Point,4326),
+deviceID integer,
+level integer,
+trigger boolean,
+msgtext varchar,
+CONSTRAINT pkey_processed_data PRIMARY KEY (pkey)
+);
