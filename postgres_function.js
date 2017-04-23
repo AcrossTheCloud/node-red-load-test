ef3888e5ed2680bb74b4261c053d17a0f86aeb32
Matthew Berryman <mattjb@me.com>
2017-04-24 08:54:51 +1000

    postgresql alternatives

--- postgres_function.js
+++
@@ -1,6 +0,0 @@
-var triggered = msg.payload.level > 100;
-var msgtext = "device " + String(msg.payload.id) + (triggered ? " triggered" : " not triggered");
-
-var querystring = "INSERT INTO processed_data (measureDateTime, location, deviceID, level, trigger, msgtext) VALUES (to_timestamp(" + String(msg.payload.time/1000) + ")," + "ST_GeomFromText('POINT(" + String( msg.payload.location.lon ) + " " + String(msg.payload.location.lat) + ")',4326)," + String(msg.payload.id) + "," + String(msg.payload.level) + ","+  String(triggered) + ",'" + msgtext + "');";
-
-return {payload: querystring};
--- schema.sql
+++ schema.sql
@@ -3,12 +3,17 @@
 CREATE EXTENSION postgis;
 
 CREATE TABLE processed_data (
-pkey serial,
-measureDateTime timestamp with time zone NOT NULL,
-location geometry(Point,4326),
-deviceID integer,
-level integer,
-trigger boolean,
-msgtext varchar,
-CONSTRAINT pkey_processed_data PRIMARY KEY (pkey)
+  pkey serial,
+  measureDateTime timestamp with time zone NOT NULL,
+  location geometry(Point,4326),
+  deviceID integer,
+  level integer,
+  trigger boolean,
+  msgtext varchar,
+  CONSTRAINT pkey_processed_data PRIMARY KEY (pkey)
 );
+
+CREATE TABLE raw_json_data (
+  pkey serial,
+  data json
+)
